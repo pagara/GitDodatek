06bb2e7bf85cd911e2f19b51c3645f6beb7df51a
Przemyslaw Gara <przemyslaw.gara@gmail.com>
2019-09-10 12:52:26 +0200

    Add project files.

---
+++ GitDodatek/Abstract/IGitData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDodatek.Abstract
+{
+    public interface IGitData
+    {
+        int GetCommits(string path);
+
+    }
+}
---
+++ GitDodatek/Inicjator.cs
@@ -0,0 +1,21 @@
+﻿using Soneta.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+[assembly: ProgramInitializer(typeof(GitDodatek.Inicjator))]
+
+
+namespace GitDodatek
+{
+    class Inicjator : IProgramInitializer
+    {
+        void IProgramInitializer.Initialize()
+        {
+
+        }
+    }
+}
